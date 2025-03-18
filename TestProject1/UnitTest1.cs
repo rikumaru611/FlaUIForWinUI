066548eaa5c64ed1c4f2e96592efd43e2bda5c1a
陸渡 田中 <r_tanaka@agenda.co.jp>
2025-03-19 08:39:13 +0900

    FluentAssertion追加しました

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -4,6 +4,7 @@ using FlaUI.Core.Input;
 using FlaUI.UIA3;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 using System.Diagnostics;
+using FluentAssertions;
 
 namespace TestProject1
 {
@@ -12,7 +13,7 @@ namespace TestProject1
         [Fact]
         public void Test1()
         {
-            var application = FlaUI.Core.Application.Launch(@"C:\Users\rikut\source\repos\TestProject1\App1\bin\x64\Debug\net8.0-windows10.0.19041.0\win-x64\App1.exe");
+            var application = FlaUI.Core.Application.Launch(@"C:\Users\r_tanaka\source\repos\TestProject1\App1\bin\x64\Debug\net8.0-windows10.0.19041.0\App1.exe");
             var mainWindow = application.GetMainWindow(new UIA3Automation());
             var cf = new ConditionFactory(new UIA3PropertyLibrary());
 
@@ -23,6 +24,7 @@ namespace TestProject1
 
             // ✅ Wait for the success dialog
             var successDialog = RetryFind(() => mainWindow.FindFirstDescendant(cf.ByAutomationId("SuccessDialog"))?.AsWindow(), 5000);
+            successDialog.Should().NotBeNull();
             //Assert.IsNotNull(successDialog);
 
             // ✅ Click OK in the dialog
